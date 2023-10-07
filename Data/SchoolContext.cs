2eb7f228f2ddde03379bed87138d5ff42c3344fb
buicuong <cuongbui21503@gmail.com>
2023-10-07 09:05:28 +0700

    thbuoi4

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Learner> Learners { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Major> Majors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Major>().ToTable(nameof(Major));
+            modelBuilder.Entity<Course>().ToTable(nameof(Course));
+            modelBuilder.Entity<Learner>().ToTable(nameof(Learner));
+            modelBuilder.Entity<Enrollment>().ToTable(nameof(Enrollment));
+        }
+    }
+}
